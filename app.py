fd5a0ade67f193334f41bfc26356cd7bc49342ae
vanessa <yu100997@gmail.com>
2014-11-10 11:35:05 -0500

    structure thing idk

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/<title>")
-def index(title="Main_Page"):
+@app.route("/tree/<node>")
+def tree(node=""):
+    pass
+
+@app.route("/page/<title>")
+def page(title="Main_Page"):
     page = ""
+    add(title)
     urlfile = urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&format=json&titles=%s&prop=links" % title)
     stuff = json.loads(urlfile.read())
     for outer in stuff["query"]["pages"]:
         page += "Links from %s:<br>\n" % stuff["query"]["pages"][outer]["title"]
         for inner in stuff["query"]["pages"][outer]["links"]:
-            page += "<a href=http://en.wikipedia.org/wiki/%s>%s</a><br>\n" % (inner["title"],inner["title"])
+            page += "<a href=%s>%s</a><br>\n" % (url_for("page", title=inner["title"]), inner["title"])
     return page
 
 if __name__ == "__main__":
